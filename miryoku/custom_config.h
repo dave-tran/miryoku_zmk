bdc1ba3b3ebc0856655792559a74bccae64a0bbc
dave-tran <45833238+dave-tran@users.noreply.github.com>
2022-11-20 14:16:13 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,10 +1,13 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-/*
+/ {
+  macros {
 ZMK_MACRO(abc,
           wait-ms = <30>;
           tap-ms = <40>;
           bindings = <&kp A &kp AT &kp B &kp C &kp RET>;
           )
-*/
+  };
+};
+
